08918cc0dbdcc9ac2b58dde6db64202bc080b294
neville777 <neville.james@student.moringaschool.com>
2023-09-27 17:16:46 +0300

    DOMcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+   let textParagraph = document.getElementById("text");
+
+   textParagraph.textContent = "This is really cool!";
+  });
+  
